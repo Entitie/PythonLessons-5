bb3943f642780a511a070994b2a1eb8c9dda1121
Entitie <entitie@yandex.ru>
2019-02-08 13:40:26 +0300

    HW done

--- Easy5-2.py
+++ Easy5-2.py
@@ -1,2 +1,10 @@
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+import os
+
+
+def dir_info():
+    print('Папки в катологе:\n' + '\n'.join([i for i in os.listdir(os.getcwd()) if os.path.isdir(i)]))
+
+
+dir_info()
