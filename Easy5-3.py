55aca44dda436905aeffe8bfad86e60bf40a8f48
Entitie <entitie@yandex.ru>
2019-02-08 13:44:44 +0300

    HW done

--- Easy5-3.py
+++ Easy5-3.py
@@ -1,2 +1,11 @@
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+import shutil
+import sys
+
+
+def file_copy():
+    print('Копия файла создана -', shutil.copyfile(sys.argv[0], sys.argv[0]+'.copy'))
+
+
+file_copy()
